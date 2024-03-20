9a054e1a9132efe743c9d09cac0fccdffaea7d85
Shamiq Raza <155806245+ShamiqRaza32@users.noreply.github.com>
2024-03-20 04:21:22 -0600

    Add files via upload

---
+++ chapter 4/app.js
@@ -0,0 +1,36 @@
+/* Question 1 */
+let a = 10,
+b = "hello",
+c = true;
+
+/* Question 2 */
+
+// Leagale variable */
+
+let name;
+let my_name;
+let _name;
+let $name;
+let name123;
+
+/* Question 3 */
+
+// A
+
+document.write ("Rules for naming js variabale");
+//B
+let nam;
+let name12;
+let my_ne;
+
+//C
+let $hello;
+let _hello;
+let hello$;
+
+//D
+//variable name are case sestitive
+
+//E
+//variable name should not be js keyword
+
